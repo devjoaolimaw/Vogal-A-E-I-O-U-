99378dc9ef1440277174ddfa630dc8f0dbb1769b
João Paulo <167019869+devjoaolimaw@users.noreply.github.com>
2024-05-27 15:15:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Vogal
+{
+  class Show
+  {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite uma letra");
+        string letra = Console.ReadLine();
+
+        switch (letra)
+        {
+            case "a":
+            case "e":
+            case "i":
+            case "o":
+            case "u":
+            Console.WriteLine("vogal");
+            break;
+
+            default:
+            Console.WriteLine("Não é uma vogal");
+            break;
+        }
+    }
+  }
+}
+
+
+
